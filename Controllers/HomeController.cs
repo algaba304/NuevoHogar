4ed00254061138a6d43dd07fef442c7910b72c72
Alberto Galindo <albertogalindobaxin2000@gmail.com>
2022-12-15 13:15:09 -0600

    consultar cuenta y cambiar contraseña finalizado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,46 +9,83 @@ public class HomeController : Controller
 {
     private readonly IHttpClientFactory? _IhttpClientFactory;
     public HomeController(IHttpClientFactory httpClientFactory){
+
         _IhttpClientFactory = httpClientFactory;
+
     }
 
     public IActionResult Index(){
+
         Cliente.Usuario = null;
         return View();
+
     }
 
     public IActionResult Privacy(){
+
         return View();
+
     }
 
     public IActionResult ErrorPagina(){
+
+        Cliente.Usuario = null;
         return View();
+
     }
 
-
-    [HttpPost]
-    public async System.Threading.Tasks.Task<IActionResult> IniciarSesion(Usuario usuario){
+    public async System.Threading.Tasks.Task<IActionResult> IniciarSesionBtn(Usuario usuario){
 
         try{
+
             var cliente = _IhttpClientFactory?.CreateClient("BackEnd");
             cliente?.DefaultRequestHeaders.Clear();
             cliente?.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
             var mensaje = await cliente?.GetAsync("api/sesion/?nombreUsuario=" + usuario.NombreUsuario + "&contrasenia=" + usuario.Contrasenia)!;
 
             if(mensaje.IsSuccessStatusCode){
+
                 string usuarioObtenido = mensaje.Content.ReadAsStringAsync().Result;
+
                 if(usuarioObtenido != null){
+
                     Usuario? usuarioEncontrado = JsonConvert.DeserializeObject<Usuario>(usuarioObtenido);
                     Cliente.Usuario = usuarioEncontrado;
+                    
                 }else{
+
                     return RedirectToAction("ErrorNoExiste", "Home");
+
                 }
+
             }else{
+
                 return RedirectToAction("ErrorPagina", "Home");
+
             }
-            return RedirectToAction("MostrarListaReportes", "Homepage");
-        }catch(Exception ex){
+
+            if(Cliente.Usuario!.Rol!.IdRol == "AD_123_R"){
+
+                return RedirectToAction("HomepageAdministrador", "Homepage");
+
+            }else if(Cliente.Usuario!.Rol!.IdRol == "AN_123_R"){
+
+                return RedirectToAction("HomepageAnimalista", "Homepage");
+
+            }else if(Cliente.Usuario!.Rol!.IdRol == "RF_123_R"){
+
+                return RedirectToAction("HomepageRefugio", "Homepage");
+
+            }
+
             return RedirectToAction("ErrorPagina", "Home");
+
+        }catch(Exception ex){
+
+            return RedirectToAction("ErrorPagina", "Home");
+
         }
+
     }
+    
 }
--- Controllers/HomepageController.cs
+++ Controllers/HomepageController.cs
@@ -7,8 +7,45 @@ namespace NuevoHogar.Controllers;
 
 public class HomepageController : Controller{
 
-    private String baseURL = "http://127.0.0.1:8080/";
-    public IActionResult MostrarListaReportes(){
-        return View();
+    public IActionResult HomepageAnimalista(){
+
+        if(Cliente.Usuario!.Rol!.IdRol == "AN_123_R"){
+
+            return View();
+
+        }else{
+
+            return RedirectToAction("ErrorPagina", "Home");
+
+        }
+        
+    }
+
+    public IActionResult HomepageRefugio(){
+
+        if(Cliente.Usuario!.Rol!.IdRol == "RF_123_R"){
+
+            return View();
+
+        }else{
+
+            return RedirectToAction("ErrorPagina", "Home");
+
+        }
+
+    }
+
+    public IActionResult HomepageAdministrador(){
+
+        if(Cliente.Usuario!.Rol!.IdRol == "AS_123_R"){
+
+            return View();
+
+        }else{
+
+            return RedirectToAction("ErrorPagina", "Home");
+
+        }
+
     }
 }
