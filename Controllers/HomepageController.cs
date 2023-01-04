6962831e70502bf3282c232042898a361ad66f2e
KristelSanchez <huachupango-@hotmail.com>
2023-01-04 07:51:55 -0600

    Se muestran los reportes y añade versión provisional de la edición y eliminación

--- Controllers/HomepageController.cs
+++ Controllers/HomepageController.cs
@@ -1,6 +1,10 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using NuevoHogar.Utils;
+using NuevoHogar.ModeloDTO;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace NuevoHogar.Controllers;
 
@@ -9,6 +13,11 @@ public class HomepageController : Controller{
     private String idAnimalista = "AN_123_R";
     private String idRefugio = "RF_123_R";
     private String idAdministrador = "AD_123_R";
+
+    private readonly IHttpClientFactory? _IhttpClientFactory;
+    public HomepageController(IHttpClientFactory httpClientFactory){
+        _IhttpClientFactory = httpClientFactory;
+    }
 
     public IActionResult Homepage(){
 
@@ -46,11 +55,28 @@ public class HomepageController : Controller{
         
     }
 
-    public IActionResult HomepageRefugio(){
+    public async System.Threading.Tasks.Task<IActionResult> HomepageRefugio(){
 
         if(Cliente.Usuario!.Rol!.IdRol == idRefugio){
+           List<Reporte2DTO> listaReportes = new List<Reporte2DTO>();
+            try{
+                var cliente = _IhttpClientFactory?.CreateClient("BackEnd");
+                cliente?.DefaultRequestHeaders.Clear();
+                cliente?.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var mensaje = await cliente?.GetAsync("api/reportes/lastReports/20")!;
 
-            return View();
+                System.Console.WriteLine(mensaje.ToString());
+
+                if(mensaje.IsSuccessStatusCode){
+                    string listaStrings = mensaje.Content.ReadAsStringAsync().Result;
+                    listaReportes = JsonConvert.DeserializeObject<List<Reporte2DTO>>(listaStrings);
+                }
+
+                return View(listaReportes);
+            }catch(Exception ex){
+                return RedirectToAction("ErrorPagina", "Home");
+            }
+            //return View();
 
         }else{
 
--- Controllers/ManejoReporteController.cs
+++ Controllers/ManejoReporteController.cs
@@ -22,6 +22,86 @@ public class ManejoReporteController : Controller{
         }
     }
 
+    public async System.Threading.Tasks.Task<IActionResult> EditReporte(string? s){
+        Reporte2DTO reporteAux = new Reporte2DTO();
+        ReporteDTO reporte = new ReporteDTO();
+        try{
+            var cliente = _IhttpClientFactory?.CreateClient("BackEnd");
+            cliente?.DefaultRequestHeaders.Clear();
+            cliente?.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var mensaje = await cliente?.GetAsync("api/reportes/" + s)!;
+
+            if(mensaje.IsSuccessStatusCode){
+                string cadena = mensaje.Content.ReadAsStringAsync().Result;
+                reporteAux = JsonConvert.DeserializeObject<Reporte2DTO>(cadena);
+                System.Console.WriteLine(reporteAux.idReporteDeMaltrato);
+            }
+            reporte = convertirReporte(reporteAux);
+            return View(reporte);
+        }catch(Exception ex){
+            return RedirectToAction("ErrorPagina", "Home");
+        }
+    }
+
+    private ReporteDTO convertirReporte(Reporte2DTO r){
+        ReporteDTO reporte = new ReporteDTO();
+        reporte.idReporteDeMaltrato = r.idReporteDeMaltrato;
+        reporte.ubicacion = r.ubicacion;
+        reporte.descripcion = r.descripcion;
+        reporte.fecha = r.fecha;
+        reporte.idRefugio = r.idRefugio;
+        reporte.idUsuario = r.idUsuario;
+        return reporte;
+    }
+
+    public async System.Threading.Tasks.Task<IActionResult> ListaReportesUsuario(){
+        List<Reporte2DTO> listaReportes = new List<Reporte2DTO>();
+        List<Reporte2DTO> listaFiltrada = new List<Reporte2DTO>();
+        try{
+            var cliente = _IhttpClientFactory?.CreateClient("BackEnd");
+            cliente?.DefaultRequestHeaders.Clear();
+            cliente?.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var mensaje = await cliente?.GetAsync("api/reportes/lastReports/20")!;
+
+            System.Console.WriteLine(mensaje.ToString());
+
+            if(mensaje.IsSuccessStatusCode){
+                string listaStrings = mensaje.Content.ReadAsStringAsync().Result;
+                listaReportes = JsonConvert.DeserializeObject<List<Reporte2DTO>>(listaStrings);
+            }
+
+            foreach(Reporte2DTO r in listaReportes){
+                if(r.idUsuario == Cliente.Usuario.IdUsuario)
+                    listaFiltrada.Add(r);
+            }
+
+            return View(listaFiltrada);
+        }catch(Exception ex){
+            return RedirectToAction("ErrorPagina", "Home");
+        }
+    }
+
+    public async System.Threading.Tasks.Task<IActionResult> ListaReportes(){
+        List<Reporte2DTO> listaReportes = new List<Reporte2DTO>();
+        try{
+            var cliente = _IhttpClientFactory?.CreateClient("BackEnd");
+            cliente?.DefaultRequestHeaders.Clear();
+            cliente?.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var mensaje = await cliente?.GetAsync("api/reportes/lastReports/20")!;
+
+            System.Console.WriteLine(mensaje.ToString());
+
+            if(mensaje.IsSuccessStatusCode){
+                string listaStrings = mensaje.Content.ReadAsStringAsync().Result;
+                listaReportes = JsonConvert.DeserializeObject<List<Reporte2DTO>>(listaStrings);
+            }
+
+            return View(listaReportes);
+        }catch(Exception ex){
+            return RedirectToAction("ErrorPagina", "Home");
+        }
+    }
+
     public async System.Threading.Tasks.Task<IActionResult> GenerarReporteBtn(ReporteDTO reporteAux){
         try{
             System.Console.WriteLine(reporteAux.fecha);
@@ -63,5 +143,7 @@ public class ManejoReporteController : Controller{
         }catch(Exception ex){
             return RedirectToAction("ErrorPagina", "Home");
         }
+
     }
+
 }
